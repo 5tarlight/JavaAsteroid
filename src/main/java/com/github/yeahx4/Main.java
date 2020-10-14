ad2b008f2ef9e8fb1c563beb6ea1d7c82df92c61
Kim Kyu San <developerstarlight@gmail.com>
2020-10-14 19:04:02 +0900

    init project

---
+++ src/main/java/com/github/yeahx4/Main.java
@@ -0,0 +1,12 @@
+package com.github.yeahx4;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDA api = JDABuilder.createDefault("NTc2NzYzNDk4NTU4NjUyNDI1.XNbO3Q.cyKz51f7RtFjlQsTFmi3mK4BgMQ").build();
+    }
+}
